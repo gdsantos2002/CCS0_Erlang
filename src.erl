77c9dee063f5f79882c5859e367d8e03429ae465
Gonçalo Santos <gdsantos.2002@gmail.com>
2023-03-14 19:17:00 +0000

    Some small tests done

--- src.erl
+++ src.erl
@@ -1,7 +1,11 @@
 -module(src).
--export([deadlock/0, rm_spaces/1, clean/1, join_words/1, clean_input/1]).
+-export([deadlock/1, rm_spaces/1, clean/1, join_words/1,
+            clean_input/1, translate/2, check_AST/1, loop/0, start/0]).
 % -import(string, [replace/4]).
 % -compile(export_all).         % avoid warnings
+
+
+start() -> spawn(src, loop, []).
 
 
 % Define an alphabet?       A = {a, b} 
@@ -19,6 +23,19 @@ join_words(List) -> lists:concat(List).
 clean_input(AST) -> join_words(clean(rm_spaces(AST))).
 % test with: src:clean_input("a.0 + b.0").
 
+% just for testing
+loop() ->
+    receive
+        {translate, From, AST} -> From ! {response, check_AST(AST)},
+        loop()
+    end.
+
+translate(Server, AST) -> Server ! {translate, self(), AST},
+    receive 
+        {response, LTS} -> LTS
+    end.
+
+check_AST("a.0 + b.0") -> true.
 
 % Deadlock case
-deadlock() -> 0.
+deadlock(zero) -> 0.
