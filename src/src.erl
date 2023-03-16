008f194e75d26bda03857fca59804dcd41bbd340
Daniel <danielsfranco18@gmail.com>
2023-03-16 21:09:36 +0000

    Created function to count final state transitions

--- src/src.erl
+++ src/src.erl
@@ -38,9 +38,13 @@ translate(Server, AST) -> Server ! {translate, self(), AST},
 
 % check_AST("a.0 + b.0") -> true. 
 
+count_final_state_transitions([], N) -> N;
+count_final_state_transitions([{_, _, "sf"}|R], N) -> count_final_state_transitions(R, N+1);
+count_final_state_transitions([_|R], N) -> count_final_state_transitions(R, N).
+
 tree(zero, List) -> List;
-tree({prefix, X, zero}, List) -> List ++ [{string:concat("s", integer_to_list(length(List))), X, "sf"}]; 
-tree({prefix, X, R}, List) -> tree(R, List ++ [{string:concat("s", integer_to_list(length(List))), X, string:concat("s", integer_to_list(length(List) + 1))}]);
+tree({prefix, X, zero}, List) -> List ++ [{string:concat("s", integer_to_list(length(List) - count_final_state_transitions(List, 0))), X, "sf"}]; 
+tree({prefix, X, R}, List) -> tree(R, List ++ [{string:concat("s", integer_to_list(length(List) - count_final_state_transitions(List, 0))), X, string:concat("s", integer_to_list(length(List) - count_final_state_transitions(List, 0) + 1))}]);
 
 tree({choice, R, zero}, List) -> tree(R, List);
 tree({choice, zero, R}, List) -> tree(R, List);
@@ -49,9 +53,9 @@ tree({choice, R, {_, Y, zero}}, List) -> L1 = tree(R, List),
     L1 ++ [{string:concat("s", integer_to_list(length(List))), Y, "sf"}];
 
 tree({choice, R, {_, Y, Rest}}, List) -> L1 = tree(R, List),
-    tree(Rest, L1 ++ [{string:concat("s", integer_to_list(length(List))), Y, string:concat("s", integer_to_list(length(L1) - length(List) + 1))}]).
-% tree({choice, C1, C2}, List) -> tree(C1, List) ++ tree(C2, List).
+    tree(Rest, L1 ++ [{string:concat("s", integer_to_list(length(List) - count_final_state_transitions(List, 0))), Y, string:concat("s", integer_to_list(length(L1) - count_final_state_transitions(L1, 0) + 1))}]).
 
 % test with: src:tree({prefix, "a", {prefix, "b", zero}}, []).
 % test with: src:tree({choice, {prefix, "a", {prefix, "b", zero}}, zero}, []).
  
+% src:tree({choice, {prefix, "a", {choice, {prefix, "c", {prefix, "e", {prefix, "h", zero}}}, {prefix, "d", {prefix, "f", {prefix, "h", zero}}}}}, {prefix, "b", {prefix, "g", zero}}}, []).
